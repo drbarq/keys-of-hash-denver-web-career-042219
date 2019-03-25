a6839f17d9dbdbaa22a5420af405823fabf13e0b
Joe Tustin <j.tustin@gmail.com>
2019-03-25 18:10:51 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,8 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    if arguments == Hash.values
+      puts Hash.Keys
     Hash.each do |animals, country|
       if country == arguments
       puts animals
