d8823aa035d4dc893ac525c86d95b6209f9b6d07
Joe Tustin <j.tustin@gmail.com>
2019-03-25 18:19:22 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,22 +1,18 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-listOfAnimals = Array.new
+    listOfAnimals = Array.new
 
     arguments.each do |arg|
       each do |animal, country|
         if country == arguments
           puts animals
           listOfAnimals << animals
+        end 
+      end
     end
-    end
-
-
   end
 end
 
 
 
-=begin
-
-=end
